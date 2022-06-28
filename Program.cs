8ddadfa13920126daafffbecf41cf50f46ab8dd3
elcanion <elcaniondev@gmail.com>
2022-06-27 23:54:23 -0300

    return Results.Ok

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => 
+{
+    var todo = new Todo(Guid.NewGuid(), "Run", false);
+    return Results.Ok();
+});
 
 app.Run();
