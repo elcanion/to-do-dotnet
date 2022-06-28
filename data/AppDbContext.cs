9a88ba28fb03f25ff04bb3b820fddc61144b7da4
elcanion <elcaniondev@gmail.com>
2022-06-28 00:03:17 -0300

    override OnConfiguring method

--- data/AppDbContext.cs
+++ data/AppDbContext.cs
@@ -4,6 +4,9 @@ namespace Todo_CSharp.data
 {
     public class AppDbContext : DbContext
     {
-        
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite("DataSource=app.db;Cache=Shared");
     }
 }
